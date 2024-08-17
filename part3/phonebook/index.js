0d8b616e121b2d0d885fddf2fe3e61dee1634d94
MihailPetkov96 <mpetkov228@gmail.com>
2024-08-17 23:54:13 +0300

    Complete exercise 3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -39,6 +39,18 @@ app.get('/api/persons', (request, response) => {
     response.json(persons);
 });
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
+    const person = persons.find(p => p.id === id);
+    
+    if (person) {
+        response.json(person);
+    } else {
+        response.status(404).end();
+    }
+    
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
