63e85e44d66292f478aae820e306737bb5276242
MihailPetkov96 <mpetkov228@gmail.com>
2024-08-18 12:28:45 +0300

    Complete exercise 3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
-const persons = [
+let persons = [
     {
         "id": "1",
         "name": "Arto Hellas",
@@ -49,6 +49,13 @@ app.get('/api/persons/:id', (request, response) => {
         response.status(404).end();
     }
     
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
+    persons = persons.filter(p => p.id !== id);
+
+    response.status(204).end();
 });
 
 app.listen(PORT, () => {
